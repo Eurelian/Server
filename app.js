7585389517ecadf8e8ad03e27a1d62e8df0b205a
Eurelian <44678944+Eurelian@users.noreply.github.com>
2021-05-27 17:08:16 +0200

    ok

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-app.get('/', (req,res)=>{
-	res.send('Hello there Cojones');
+app.get("/", (req, res) => {
+  res.send("Hello there Cojones");
 });
 
-app.listen(port, ()=>console.log(`Ready to rumble on port ${port}`));
+app.get("/other", (req, res) => {
+  res.send("Wow this works too");
+});
+
+app.listen(port, () => console.log(`Ready to rumble on port ${port}`));
